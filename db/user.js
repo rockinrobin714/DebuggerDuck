01a2ee409a54be8669be0710fb474e0156802432
Robin Dykema <robindykema@gmail.com>
2016-12-01 12:35:41 -0600

    Added user group karma table

--- db/user.js
+++ db/user.js
@@ -1,5 +1,6 @@
 const mongoose = require ('mongoose');
 const Schema = mongoose.Schema;
+mongoose.connect('mongodb://localhost/27017');
 //mongoose.connect(172.23.0.0/16)
 
 const UserSchema = new Schema ({
@@ -28,5 +29,4 @@ UserSchema.pre('save', function(next) {
 
 const User = mongoose.model('user', UserSchema);
 
-//
 module.exports = User;
