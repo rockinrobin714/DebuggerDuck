60b0ea7c3a2155f0266f1bed6a9d6b2c5dde7e08
Robin Dykema <robindykema@gmail.com>
2016-12-01 13:44:45 -0600

    Folder restructure

---
+++ server/db/test/create_test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const User = require('../src/user')
+//done is available for any 'it' or 'before each'
+//You MUST call done if you put it in the parens
+describe('Creating records', () => {
+	it ('saves a user', (done) => {
+		const joe = new User({name:'Joe'});
+
+		joe.save()
+			.then(()=>{
+				//Has Joe been saved sucessfully?
+				assert(!joe.isNew);
+				done();
+			})
+	});
+});
