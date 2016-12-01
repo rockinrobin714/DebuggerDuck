6c35dbee50dd9a74844d30e73ef8075dabf2f528
Johnny McDuff <jtmcduff@gmail.com>
2016-12-01 12:32:24 -0600

    Checking stubs are committed.

---
+++ client/src/Request.js
@@ -0,0 +1,18 @@
+//Request.js displays existing food request.
+//Renders once per food request, under the relevant Volunteer.
+
+//Standard Imports.
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Component
+const Request = () => {
+  return ( 
+   <div>
+    'Request'
+     
+  </div>
+  );
+};
+
+export default Request;
