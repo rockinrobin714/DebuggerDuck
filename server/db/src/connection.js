c97ad00662c07c10caa5319426ae79cc2c6db6d6
Robin Dykema <robindykema@gmail.com>
2016-12-01 23:48:58 -0600

    Restructured folders and cleaned up connection code

--- server/db/src/connection.js
+++
@@ -1,3 +0,0 @@
-const mongoose = require ('mongoose');
-const connection = mongoose.connect('mongodb://debuggerduck:debuggerduck1@ds119588.mlab.com:19588/debuggerduck');
-module.exports = connection;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./util/router.js');
-const db = require('./db/src/schemas.js')
-const dbConnection = require('./db/src/connection.js')
+const db = require('./db/schemas.js')
+const dbConnection = require('./db/connection.js')
 
 // Use express
 const app = express();
 module.exports.app = app;
 
+
+//***How to insert something in the DB:
+// let example = new db.user({ username:'blablabla', pic:'blablabla' });
+// example.save();
+
+
+
 // Use body-parser for parsing JSON in the request body
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 // Check to see if there is a port environment variable or just use port 4040 instead
 const port = process.env.PORT || 4040;
