e000eb779380d62575df4b5a1aac37e028cd5fb3
Robin Dykema <robindykema@gmail.com>
2016-12-02 12:28:51 -0600

    Updated schemas

--- server/db/schemas.js
+++ server/db/schemas.js
@@ -5,15 +5,15 @@ const Schema = mongoose.Schema;
 let db = {};
 
 const UserSchema = new Schema ({
-	//mongoose will automatically create a unique id, so no need to store one
+	//mongoose will automatically create a unique id, so no need to manually create one
 	username: String,
 	picture: String,
-	groups: [{group_id: String, karma:{type: String, default:0}}]
+	groups: [{group_id: String, karma:{type: Number, default:0}}]
 
 });
 
 // let getNameFromFb = function(input){
-// 	//TODO: Using fbs api, retrieve the name from fb
+// 	//TODO?: Using fbs api, retrieve the name from fb
 // 	return input;
 // }
 // let getPicFromFb = function(input){
@@ -29,39 +29,22 @@ const UserSchema = new Schema ({
 //   next();
 // });
 
-const UserGroupKarmaSchema = new Schema ({
-//mongoose will automatically create a unique id, so no need to store one
-//This username is connected to the users table
-	user_id: String,
-	//This group id is connected to the group table
-	group_id: String,
-	//Set the default amount of karma to 0
-	karma: {type: String, default:0}
-
-});
-
-//Not sure if this belongs here. If so, fill it out.
-//If not, delete it.
-
-// let setUpKarma = function(input){
-// 	TODO? By querying the database, update the karma
-// }
-
-
-// UserGroupKarmaSchema.pre('save', function(next) {
-//   next();
-// });
-
 const GroupSchema = new Schema ({
 	//Will automatically generate group id
 	name: String
 })
 
-const GroupUserOrderSchema = new Schema ({
-	//This has group_id
+const OrderSchema = new Schema ({
+	//Will automatically generate order id
+	createdAt: { type : Date, default: Date.now },
+	location: String,
+	time: String,
+	group_id: String,
+	requests: [{user_id: String, text: String}]
 })
+
 db.user = mongoose.model('user', UserSchema);
-db.userGroupKarma = mongoose.model('userGroupKarma', UserGroupKarmaSchema);
-db.group = mongoose.model('group',GroupSchema)
+db.group = mongoose.model('group', GroupSchema);
+db.order = mongoose.model('order', OrderSchema);
 
 module.exports = db;
