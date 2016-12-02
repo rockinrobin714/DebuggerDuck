aa97be8ecec942625626c406eea33b81f92ef7ae
Robin Dykema <robindykema@gmail.com>
2016-12-02 15:14:03 -0600

    Playing with login button

--- client/src/Login.js
+++ client/src/Login.js
@@ -2,56 +2,15 @@
 import React, {Component} from 'react';
 
 class FacebookButton extends Component {
-  constructor(props) {
-    super(props);
-    this.FB = props.fb;
-    this.state = {
-      message: ""
-    };
-  }
-
-  componentDidMount() {
-    this.FB.Event.subscribe('auth.logout', 
-    this.onLogout.bind(this));
-    this.FB.Event.subscribe('auth.statusChange', 
-    this.onStatusChange.bind(this));
-  }
-      
-  onStatusChange(response) {
-    console.log( response );
-    var self = this;
-
-    if( response.status === "connected" ) {
-      this.FB.api('/me', function(response) {
-        var message = "Welcome " + response.name;
-        self.setState({
-          message: message
-        });
-      })
-    }
-  }
-
-  onLogout(response) {
-    this.setState({
-      message: ""
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <div 
-        className="fb-login-button" 
-        data-max-rows="1" 
-        data-size="xlarge" 
-        data-show-faces="false" 
-        data-auto-logout-link="true"
-        >
-        </div>
-        <div>{this.state.message}</div>
+   render(){
+      return (
+         <div>
+      <h1> Here is a facebook button!</h1>
+      <button className='.btn-primary'><i className="fa fa-facebook" aria-hidden="true"></i> Log in to facebook </button>
       </div>
-    );
-    }
-  };
+      )
+   }
+   
+};
 
 export default FacebookButton;
