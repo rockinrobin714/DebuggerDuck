edfa3f6bd6171618b04af1fe17b1c86cc1a17f08
Robin Dykema <robindykema@gmail.com>
2016-12-02 17:00:26 -0600

    MERGING IS THE WORST

--- client/src/Request.js
+++ client/src/Request.js
@@ -9,7 +9,7 @@ import ReactDOM from 'react-dom';
 class Request extends Component {
 	constructor(props) {
     super(props);
- 
+
     this.state = {person: '', request: ''};
   }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
-import FacebookButton from './Login.js'
+
 
 ReactDOM.render(
-  //<App />,
-  <Runner />,
+
+  <App />,
   document.getElementById('root')
 );
