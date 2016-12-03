063da96b17c983ff11e9759628a8f9669f5a1924
Robin Dykema <robindykema@gmail.com>
2016-12-03 17:27:24 -0600

    Fb button now alters state

--- client/src/Login.js
+++ client/src/Login.js
@@ -4,25 +4,14 @@ import React, {Component} from 'react';
 class FacebookButton extends Component {
    constructor(props) {
     super(props);
+    this.state = {};
+  }
 
-    this.state = {
-      loggedIn: false
-    };
-  }
-  componentDidMount() {
-    // Not sure how to do this yet
-    // axios.get(`/fb`)
-    //   .then(res => {
-    //     const projects = res.data
-    //   	this.setState(loggedIn:true);
-    //   });
-  }
 //fb icon = <i className="fa fa-facebook" aria-hidden="true"></i> Log in with facebook
    render(){
-      let loggedInText = 'Log in with facebook';
-      let loggedOutText = 'Log out';
+      
       return (
-         <button className='fb-button btn' style={{float:"right"}}>{loggedInText}</button>
+         <button onClick={this.props.onButtonClick} className='fb-button btn'><i className="fa fa-facebook" aria-hidden="true">  </i>{this.props.text}</button>
       )
    }
    
--- client/src/Profile.js
+++ client/src/Profile.js
@@ -10,16 +10,52 @@ import FacebookButton from './Login.js'
 
 //Component
 class Profile extends Component {
-  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loggedIn: false,
+      karma: 0
+    };
+  }
+  componentDidMount() {
+    // Not sure how to do this yet
+    // axios.get(`/fb`)
+    //   .then(res => {
+    //     const projects = res.data
+    //   	this.setState(loggedIn:true);
+    //   });
+  }
+  onButtonClick(){
+  	//this.setState({!this.state.loggedIn})
+  	if (this.state.loggedIn===true){
+	      this.setState({
+	        loggedIn: false
+	      })
+	    } else {
+	      this.setState({
+	        loggedIn: true
+	      })
+	    }
+	  }
 
   render() { 
-    return ( 
+  	if(this.state.loggedIn===false){
+  		return (
+  			<div className='nav-bar'>
+  				
+    			<FacebookButton onButtonClick={this.onButtonClick.bind(this)} text={'Sign up with Facebook'}/>
+    		</div>
+    	)
+  	} else {
+  		return ( 
     	<div className='nav-bar'>
-    	<FacebookButton />
+    		<FacebookButton onButtonClick={this.onButtonClick.bind(this)} text={'Log out'}/>
+    		<div className='karma'>Karma:{this.state.karma}</div>
     	</div>
-  );
- }
-  
+  		);
+  	}
+  	}   
 };
 
 
