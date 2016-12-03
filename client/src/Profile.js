99319fb8c2ec016a3a1823b0b2f2a237ce2a4ffe
Robin Dykema <robindykema@gmail.com>
2016-12-02 20:27:32 -0600

    Finished with style for the night

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -13,28 +13,7 @@ class Profile extends Component {
 
   render() { 
     return ( 
-    	TODO:
-    	<div class="navbar navbar-default navbar-static-top">
-	  		<div class="container">
-	    		<div class="navbar-header">
-	      			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-	        		<span class="icon-bar"></span>
-	        		<span class="icon-bar"></span>
-	        		<span class="icon-bar"></span>
-	      			</button>
-	      			<a class="navbar-brand" href="#">Food Runner</a>
-	    		</div>
-	    	<div class="collapse navbar-collapse">
-	      		<ul class="nav navbar-nav navbar-right">
-			        <li>Rubber Duck Debugger</li>
-			        <li>Karma: 0</li>
-			        <li>Logout</li>
-			    </ul>
-	    	</div>
-	    </div>
-	    </div>
-       // <img className='main-profile-pic'
-       // src="https://static01.nyt.com/images/2011/11/13/magazine/13duck/13duck-jumbo.jpg"/>
+    	<div>hi</div>
   );
  }
   
